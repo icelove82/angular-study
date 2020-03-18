a824cb94e5e29ce710b3ae02b20b9cfa100eda1d
icelove82 <yunmyeonghun@gmail.com>
2020-03-18 09:06:34 +0900

    Add first input & btn.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
+  helloMsg = 'Hello, world.';
+
+  handleClick(event, inputVal) {
+    window.alert("您点击了: " + event.target + ", 您输入的内容是: " + inputVal);
+  }
 }
