ad2954f9db7833e0053a5e7063e483a75945d670
icelove82 <yunmyeonghun@gmail.com>
2020-03-24 19:28:58 +0900

    Remove unuse.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { ButtonModule } from 'primeng/button';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +7,4 @@ import { ButtonModule } from 'primeng/button';
 })
 export class AppComponent {
   title = 'my-first-app';
-  helloMsg = 'Hello, world.';
-
-  handleClick(event, inputVal) {
-    window.alert("您点击了: " + event.target + ", 您输入的内容是: " + inputVal);
-  }
 }
