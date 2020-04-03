0989736ac45a07f62142ed825d2e5c680363ad2e
icelove82 <yunmyeonghun@gmail.com>
2020-04-03 19:07:19 +0900

    Add provideIn root

--- src/app/car-service.service.ts
+++ src/app/car-service.service.ts
@@ -1,23 +1,23 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 // Car interface
-import { Car } from './list/list.component';
+import { Car } from "./list/list.component";
 
-@Injectable()
+@Injectable({ providedIn: "root" })
 export class CarServiceService {
   constructor(private http: HttpClient) {}
 
   getCarsSmall() {
     return this.http
-      .get('../assets/car.json')
+      .get("../assets/car.json")
       .toPromise()
       .then(res => {
-        return <Car[]> res // type casting
+        return <Car[]>res; // type casting
       })
       .then(data => {
         return data;
-      })
-      .catch(e => console.log('Get error', e));
+      });
+    // .catch(e => console.log('Get error', e));
   }
 }
