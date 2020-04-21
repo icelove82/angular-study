5bb95283229d2b2bfacac91b29aea4c93c9e38ef
icelove82 <yunmyeonghun@gmail.com>
2020-04-21 20:52:04 +0900

    Add dropdown

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 import { InputTextModule } from 'primeng/inputtext';
+import { SelectItem } from 'primeng/api';
+
+interface Sex {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-
   // 组件主数据
   name;
+  gender: Sex;
 
   // 组件非主数据
   namePlaceholder = '请输入名字';
+  genderList: Sex[];
 
-  constructor() { }
+  constructor() {
+    this.genderList = [
+      { name: '男', code: 'M' },
+      { name: '女', code: 'F' }
+    ];
 
-  ngOnInit(): void {
+    this.gender = this.genderList[0];
   }
+
+  ngOnInit(): void {}
 
   onBlur() {
-    alert('The value :' + this.name);
+    alert('The value :' + this.name + ' / Gender : ' + this.gender.name);
   }
-
 }
